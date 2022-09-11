8a4856f2241025174f11041d20ab3567c2fcb99e
Pavel Tropinov <pavel.tropinov@gmail.com>
2022-09-11 19:46:21 +0300

    Added managing script for Ubuntu-based systems

---
+++ manager.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+APP_NAME=AutoHeadHunterResumeUpper
+SERVICE_NAME="${APP_NAME}_SERVICE"
+PATH_TO_JAR="./${APP_NAME}.jar"
+PID_PATH_NAME="/tmp/${APP_NAME}-pid"
+case $1 in
+    start)
+        echo "Starting $SERVICE_NAME ..."
+        if [ ! -f $PID_PATH_NAME ]; then
+            nohup java -jar $PATH_TO_JAR >> logs.txt 2>&1&
+            echo $! > $PID_PATH_NAME
+            echo "$SERVICE_NAME started ..."
+        else
+            echo "$SERVICE_NAME is already running ..."
+        fi
+    ;;
+    stop)
+        if [ -f $PID_PATH_NAME ]; then
+            PID=$(cat $PID_PATH_NAME);
+            echo "$SERVICE_NAME stopping ..."
+            kill $PID;
+            echo "$SERVICE_NAME stopped ..."
+            rm $PID_PATH_NAME
+        else
+            echo "$SERVICE_NAME is not running ..."
+        fi
+    ;;
+    restart)
+        if [ -f $PID_PATH_NAME ]; then
+            PID=$(cat $PID_PATH_NAME);
+            echo "$SERVICE_NAME stopping ...";
+            kill $PID;
+            echo "$SERVICE_NAME stopped ...";
+            rm $PID_PATH_NAME
+            echo "$SERVICE_NAME starting ..."
+            nohup java -jar $PATH_TO_JAR >> logs.txt 2>&1&
+            echo $! > $PID_PATH_NAME
+            echo "$SERVICE_NAME started ..."
+        else
+            echo "$SERVICE_NAME is not running ..."
+        fi
+    ;;
+esac
